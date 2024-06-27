2520589c5157ae7593a2adcaf22652e9f3ba8a56
levites-wads <wadingalevites4@gmail.com>
2024-06-27 17:03:56 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// App.js
+
+import React from 'react';
+import SimpleComponent from './component/filecoding'; // Import your component
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+     <SimpleComponent />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/component/filecoding.jsx
@@ -0,0 +1,15 @@
+// SimpleComponent.js
+
+import React from 'react';
+
+// Functional component definition
+function SimpleComponent() {
+  return (
+    <div>
+      <h2>This is a Simple Component</h2>
+      <p>Hello, I am a simple React component!</p>
+    </div>
+  );
+}
+
+export default SimpleComponent;
